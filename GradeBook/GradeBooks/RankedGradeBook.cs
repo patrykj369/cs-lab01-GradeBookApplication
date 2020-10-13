006577a50edea4b3b770848d260f47c6cb69d84d
Patryk Jabłoński <patryk.jablonski@microsoft.wsei.edu.pl>
2020-10-13 22:45:40 +0200

    CalculateStudentStatistics()

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -51,10 +51,19 @@ namespace GradeBook.GradeBooks
             if(Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
             }
                 base.CalculateStatistics();
+        }
 
-            
+        public override void CalculateStudentStatistics(string name)
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
         }
     }
 }
