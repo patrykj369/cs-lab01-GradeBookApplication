c0f2c6a58a4d0cad850b90bcb18ce70f954f4595
Patryk Jabłoński <patryk.jablonski@microsoft.wsei.edu.pl>
2020-10-13 22:38:26 +0200

    GetLetterGrade() and CalculatedStatistics()

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace GradeBook.GradeBooks
 {
@@ -17,9 +20,39 @@ namespace GradeBook.GradeBooks
             {
                 throw new InvalidOperationException("Ranked grading requires at least 5 students.");
             }
-            
-           
 
+            var level = (int)Math.Ceiling(Students.Count * 0.2); //zwraca liczbe calkowita ilosci studentow podzielona przez 5(20%)
+            var newList = Students.OrderByDescending(x => x.AverageGrade).Select(x => x.AverageGrade).ToList(); //tworzy newList(liste) z posortowanymi ocenami malejaco
+
+            if(newList[level - 1] <= grade) //jezeli ocena wejsciowa jest wieksza od elementu w liscie rownemu 20% ilosci studentow - 1 to zwraca A
+            {
+                return 'A';
+            }
+            else if (newList[(level * 2) - 1] <= grade)
+            {
+                return 'B';
+            }
+            else if(newList[(level * 3) - 1] <= grade)
+            {
+                return 'C';
+            }
+            else if(newList[(level * 4) - 1] <= grade)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
+
+        public override void CalculateStatistics()
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+                base.CalculateStatistics();
 
             
         }
